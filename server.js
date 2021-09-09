e626384d373d9c54604dbece10cd8d8e9920caa0
Ronak Shethia <jshethr@jci.com>
2021-09-09 13:14:20 +0530

    Google Auth Api Fixeds

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const { auth } = require('firebase-admin');
 const https = require('https');
 const fs = require('fs');
 const path = require("path");
-const isLocal = true;
+const isLocal = false;
 
 // catch unexpected exception becuase of which server get crashed
 process.on('uncaughtException', (uncaughtExc) => {
@@ -186,5 +186,5 @@ const setupServer = (isClusterRequired) => {
   }
 };
 
-setupServer(false);
+setupServer(true);
 
